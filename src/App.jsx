e2db725019bc4c7281ee86c1f3170eb0a6b6148a
alexandrtovmach <alexandrtovmach@gmail.com>
2021-10-27 16:41:55 +0300

    temp: Forced wrong code to test CI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import SearchPage from "./pages/Search"
+import SearchPage from "./pages/Search";
 
 const App = () => {
+  console.log('logs are not allowed in final commit')
   return <SearchPage />
 }
 
-export default App
+export default App;
