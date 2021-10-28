2535df134989b6cd9f2521104788636248759213
fominmaksim <fomin.max.13@gmail.com>
2021-10-28 09:44:36 +0300

    feat: created search feature

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,16 @@
+import styled from "styled-components"
+
 import Layout from "../containers/Layout"
 import SearchContainer from "../containers/Search"
+
+const StyledH1 = styled.h1`
+
+`
 
 const SearchPage = () => {
   return (
     <Layout>
+      <StyledH1>Stellar lets you plan the most amazing trips of your lifetime ever  </StyledH1>
       <SearchContainer />
     </Layout>
   )
