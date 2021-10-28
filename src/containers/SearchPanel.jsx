045db0bf0e0a5d4ded2f425bbf0bbd9de5f39146
fominmaksim <fomin.max.13@gmail.com>
2021-10-28 12:28:06 +0300

    Feat: added html and styles

--- src/containers/SearchPanel.jsx
+++ src/containers/SearchPanel.jsx
@@ -1,8 +1,32 @@
+import styled from "styled-components"
+
+const SearchPanelSection = styled.div`
+  display: flex;
+`
+const SearchPanelFrom = styled.div`
+  margin-right: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+const SearchPanelTo = styled(SearchPanelFrom)``
+
+const StyledP = styled.p`
+  color: gray;
+`
+
 const SearchPanel = () => {
   return (
-    <div>
-      <input type='text' placeholder='Search your next fly ticket' />
-    </div>
+    <SearchPanelSection>
+      <SearchPanelFrom>
+        <StyledP>From</StyledP>
+        <input type='text' placeholder='Kryvyi Rih' />
+      </SearchPanelFrom>
+      <SearchPanelTo>
+        <StyledP>To</StyledP>
+        <input type='text' placeholder='San Francisco' />
+      </SearchPanelTo>
+    </SearchPanelSection>
   )
 }
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,15 +3,39 @@ import styled from "styled-components"
 import Layout from "../containers/Layout"
 import SearchContainer from "../containers/Search"
 
-const StyledH1 = styled.h1`
+const StyledP = styled.p`
+  color: white;
+`
 
+const StyledH1 = styled.h1`
+  color: white;
+`
+
+const SearchSection = styled.div`
+  background: black;
+  width: 100%;
+  height: 100%;
+`
+const SearchSectionContent = styled.div`
+  margin: 10px 100px;
 `
 
 const SearchPage = () => {
   return (
     <Layout>
-      <StyledH1>Stellar lets you plan the most amazing trips of your lifetime ever  </StyledH1>
-      <SearchContainer />
+      <SearchSection>
+        <SearchSectionContent>
+          <StyledH1>
+            Stellar lets you plan the most amazing trips <br /> of your lifetime ever, easily.
+          </StyledH1>
+          <SearchContainer />
+          <StyledP>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            <br /> Dicta id laboriosam temporibus ipsa, facere totam asperiores, quis voluptates quasi consequatur
+            laborum! Impedit nobis!
+          </StyledP>
+        </SearchSectionContent>
+      </SearchSection>
     </Layout>
   )
 }
